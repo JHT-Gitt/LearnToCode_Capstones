6dc390a1cab4c4b54c2cb951cfb6558a43e32ec2
Jiward Torres <jiwardtorres@yahoo.com>
2025-04-25 16:17:04 -0700

    new

--- FinancialTracker/src/main/java/org/example/Entries.java
+++ FinancialTracker/src/main/java/org/example/Entries.java
@@ -4,11 +4,20 @@ public class Entries {
     private String description;
     private String vendor;
     private float amount;
+    private String date,time;
 
     public Entries(String description, String vendor, float amount) {
         this.description = description;
         this.vendor = vendor;
         this.amount = amount;
+    }
+
+    public Entries(String date, String time, String description, String vendor, float amount) {
+        this.description = description;
+        this.vendor = vendor;
+        this.amount = amount;
+        this.date = date;
+        this.time = time;
     }
 
     public String getDescription() {
@@ -33,5 +42,21 @@ public class Entries {
 
     public void setAmount(float amount) {
         this.amount = amount;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
     }
 }
--- FinancialTracker/src/main/java/org/example/Tracker.java
+++ FinancialTracker/src/main/java/org/example/Tracker.java
@@ -1,20 +1,95 @@
 package org.example;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 public class Tracker {
 
-    ArrayList<Entries> entry = new ArrayList<>();
-
-
+    public static Scanner scanner = new Scanner(System.in);
+    public static ArrayList<Entries> entry = new ArrayList<>();
 
     public static ArrayList<Entries> showEntry(){
 
         try{
+            BufferedReader buff = new BufferedReader(new FileReader("transactions.csv"));
+            String line;
+            while((line = buff.readLine()) !=null){
+                String[] token = line.split("\\|");
+                if(token.length == 5){
+                    String date = token[0];
+                    String time = token[1];
+                    String desc = token[2];
+                    String vendor = token[3];
+                    float amount = Float.parseFloat(token[4]);
 
+                    Entries entries = new Entries(date,time,desc,vendor,amount);
+                    entry.add(entries);
+                }
+            }
+    buff.close();
         } catch (Exception e) {
             System.out.println("\nError ! Something went wrong.");
         }
+        return entry;
+    }
+
+    public static void menu(){
+        String input;
+        boolean isTrue = false;
+        System.out.println("\n--- HOME SCREEN ---");
+        System.out.println(" D - Add Deposit");
+        System.out.println(" P - Make Payment(Debit)");
+        System.out.println(" L - Ledger");
+        System.out.println(" X - Exit");
+        while(!isTrue) {
+            try {
+                System.out.print("\nEnter your choice: ");
+                input = scanner.next().toLowerCase();
+
+                if(input.equalsIgnoreCase("D")){
+                    addDeposit();
+                }else if (input.equalsIgnoreCase("P")){
+                    makePayment();
+                }else if (input.equalsIgnoreCase("L")) {
+                    ledger();
+                }else if(input.equalsIgnoreCase("X")){
+                    System.out.println("\nGoodbye");
+                    System.exit(0);
+                }else{
+                    System.out.println("Invalid input");
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("\nError ! Invalid Input !");
+                scanner.nextLine();
+            }
+        }
+    }
+    public static void addDeposit(){
+        System.out.println("add nah yet");
+        menu();
+    }
+    public static void makePayment(){
+        System.out.println("pay,emt nah yet");
+        menu();
+    }
+    public static void ledger(){
+        System.out.println("ledger nah yet");
+        menu();
+    }
+    public static void showAllEntry(){
+
+    }
+    public static void showDeposits(){
+
+    }
+    public static void showPayments(){
+
+    }
+    public static void showReports(){
+
     }
 
 
